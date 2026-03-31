b24d4f2d91103e01412e1397857b44eee8e1ea51
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-31 21:10:14 +0530

    Refactor digital clock code in index.py

--- index.py
+++ index.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import datetime
-
 root = Tk()
 root.geometry("400x400")
 root.config(bg="black")
@@ -17,7 +16,6 @@ def clock():
 
     label.config(text=current_time + "\n" + current_date)
     label.after(1000,clock)
-
 
 label = Label(design, bg="black", fg="cyan",font=("Arial", 35, "bold"))
 label.place(relx=0.5, rely=0.5, anchor="center")
