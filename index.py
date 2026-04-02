b394e034c09103ebf71838aff73ce7093cc5bb78
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-04-02 20:43:30 +0530

    Restore digital clock functionality
    Restored the digital clock functionality in index.py.

--- index.py
+++ index.py
@@ -13,12 +13,3 @@ def clock():
     now = datetime.datetime.now()
     current_time = now.strftime("%I:%M:%S %p")
     current_date = now.strftime("%A\n%d %B %Y")
-
-    label.config(text=current_time + "\n" + current_date)
-    label.after(1000,clock)
-
-label = Label(design, bg="black", fg="cyan",font=("Arial", 35, "bold"))
-label.place(relx=0.5, rely=0.5, anchor="center")
-
-clock()
-root.mainloop()
